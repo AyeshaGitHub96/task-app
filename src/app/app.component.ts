e3b87fcaed4e42080203ce475d7d8dede31fda2d
Ayesha GitHub <ayeshanmalee@gmail.com>
2025-03-11 09:41:38 +0530

    auth config-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { CreateTaskComponent } from "./compoenets/create-task/create-task.component";
 import { TaskListComponent } from "./compoenets/list-task/list-task.component";
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule} from '@angular/material/tabs';
+
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,5 +16,21 @@ import {MatTabsModule} from '@angular/material/tabs';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'testapp';
+
+  // isLoggedIn = false;
+
+  // constructor(private authService: AuthService, private router: Router) {
+  //   this.authService.getAuthState().subscribe(user => {
+  //     this.isLoggedIn = !!user;
+  //     if (!this.isLoggedIn) {
+  //       this.router.navigate(['/login']);
+  //     }
+  //   });
+  // }
+
+  // logout(): void {
+  //   this.authService.logout().subscribe(() => {
+  //     this.router.navigate(['/login']);
+  //   });
+  // }
 }
